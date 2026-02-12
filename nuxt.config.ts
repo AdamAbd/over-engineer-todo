1018704eeda1791372d8cb4737fe792681d05809
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2026-02-12 18:17:22 +0700

    feat: integrate Tailwind CSS and update configuration in nuxt.config.ts, package.json, and add main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,4 +11,8 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@nuxt/image',
   ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
