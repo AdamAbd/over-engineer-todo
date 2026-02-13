427791c58cb523c599a0cee55216d3b97dc9b9c8
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2026-02-13 13:25:05 +0700

    feat: update Nitro preset configuration to use cloudflare-module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
-    preset: 'cloudflare',
+    preset: 'cloudflare-module',
   },
   modules: [
     '@nuxt/eslint',
